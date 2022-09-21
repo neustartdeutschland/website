145f71bc630c4cc204c6cc64d3b67c5de9ca27da
Martin Skroch <martin.sk@13-grad.com>
2022-09-21 13:41:39 +0200

    Added Piwik

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
+import mixpanel from 'mixpanel-browser'
 import { createApp } from 'vue'
 import './scss/main.scss'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+mixpanel.init('5c73b1ac4543da3cc393030a374a273c');
+mixpanel.track('Sign up');
